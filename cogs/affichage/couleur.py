435cc6e15b9c1885894df815a4c41b03b1be5704
Ventona666 <58355856+Ventona666@users.noreply.github.com>
2020-11-07 14:27:15 +0100

    Add files via upload

---
+++ cogs/affichage/couleur.py
@@ -0,0 +1,10 @@
+class Couleur():
+    """ Classe définissant certains paramètres utilisés par les embeds, à savoir :
+        - L'adresse url du portrait de Eve, un en bleu et l'autre en rouge
+        - Le code couleur utilisé pour définir le bleu et le rouge """
+
+    def __init__(self):
+        self.blueImageEve = 'https://cdn.discordapp.com/attachments/712966107916664852/766677614692663336/fsdf.jpg'
+        self.blueColor = 0x1DA1F2
+        self.redImageEve = 'https://cdn.discordapp.com/attachments/712966107916664852/766677614692663336/fsdf.jpg'
+        self.redColor = 0xF209FE
--- cogs/affichage/embed.py
+++ cogs/affichage/embed.py
@@ -1,1 +1,17 @@
+import discord
+from discord.ext import commands
+from discord.utils import get
 
+
+class Embed():
+    """ Classe définissant l'embed, à savoir  :
+        - La couleur
+        - Le titre
+        - La description
+        - L'url de l'image à insérer
+        - L'url du lien sur doit être redirigé la personne lorsqu'elle clique sur le titre """
+
+    def __init__(self, color, title, description, url):
+        self.format = discord.Embed(color=color, title=title, description=description)
+        self.format.set_thumbnail(url=url)
+        self.format.set_footer(text="Extended Virtual Entity - Version 3.1")
